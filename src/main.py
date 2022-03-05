7d61693ebed414077c7fc44461903a03ec33cf62
DiceNameIsMy <dicenameismy@gmail.com>
2022-03-05 18:21:47 +0600

    feat: CORS middleware

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from settings import settings
 
@@ -6,6 +7,14 @@ from api.v1.router import router as v1_router
 
 
 app = FastAPI(title=settings.project_name)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.allowed_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(v1_router, prefix="/v1", tags=["v1"])
 
 
