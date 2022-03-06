3a865e401df8bfba304170ade1218e44be93a870
DiceNameIsMy <dicenameismy@gmail.com>
2022-03-06 16:43:53 +0600

    feat: make user model fields not nullable

--- src/repository/models.py
+++ src/repository/models.py
@@ -6,7 +6,7 @@ from .database import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String(127), unique=True)
-    password = Column(String(255))
-    is_active = Column(Boolean(), default=True)
+    id = Column(Integer, primary_key=True)
+    email = Column(String(127), unique=True, nullable=False)
+    password = Column(String(255), nullable=False)
+    is_active = Column(Boolean(), default=True, nullable=False)
