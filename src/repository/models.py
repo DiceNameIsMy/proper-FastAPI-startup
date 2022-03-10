39c042fb5e2aa93fd8f87d87f390c370a8c46cfd
DiceNameIsMy <dicenameismy@gmail.com>
2022-03-10 15:53:32 +0600

    feat: add verification code model

--- src/repository/models.py
+++ src/repository/models.py
@@ -1,4 +1,14 @@
-from sqlalchemy import String, Column, Integer, Boolean
+from sqlalchemy import (
+    String,
+    Column,
+    Integer,
+    DateTime,
+    Boolean,
+    ForeignKey,
+    UniqueConstraint,
+    Index,
+)
+from sqlalchemy.orm import relationship
 
 from .database import Base
 
@@ -10,3 +20,21 @@ class User(Base):
     email = Column(String(127), unique=True, nullable=False)
     password = Column(String(255), nullable=False)
     is_active = Column(Boolean(), default=True, nullable=False)
+    is_email_verified = Column(Boolean(), default=False, nullable=False)
+
+    verification_codes = relationship("VerificationCode", back_populates="user")
+
+
+class VerificationCode(Base):
+    __tablename__ = "verification_codes"
+    __table_args__ = (
+        UniqueConstraint("user_id", "code"),
+        Index("verification_codes_user_id_idx", "user_id", "code"),
+    )
+
+    id = Column(Integer, primary_key=True)
+    code = Column(Integer, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    expires_at = Column(DateTime(timezone=True), nullable=False)
+
+    user = relationship("User", back_populates="verification_codes")
