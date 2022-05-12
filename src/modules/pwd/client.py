e4a53133eae06e729900033f88952d4c9c47a446
Nurdaulet <dicenameismy@gmail.com>
2022-05-12 14:12:27 +0600

    fix: error on login with password after google signup

--- src/modules/pwd/client.py
+++ src/modules/pwd/client.py
@@ -1,11 +1,15 @@
 from passlib.context import CryptContext
+from passlib.exc import UnknownHashError
 
 
 class PwdClient:
     pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
     def verify_password(self, plain_password: str, hashed_password: str) -> bool:
-        return self.pwd_context.verify(plain_password, hashed_password)
+        try:
+            return self.pwd_context.verify(plain_password, hashed_password)
+        except UnknownHashError:
+            return False
 
     def get_password_hash(self, password: str) -> str:
         return self.pwd_context.hash(password)
